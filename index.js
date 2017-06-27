86440c416ed850d22b904b2bc66a998f465f3c3c
Rathi <github email address>
2017-06-27 11:12:13 -0400

    few left

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-// Code your solution here!
 
+function printString(myString) {
+  console.log(myString[0]);
+
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+
+function reverseString(str) {
+  if (str.length < 2){
+    return str ;
+  }else {
+    return reverseString(str.substring(1)) + str[0] ;
+  }
+}
+
+function isPalindrome(str){
+  let str1 = str
+  if (str.length < 2){
+    return str ;
+  }else if (str.length <= 1) {
+    return
+
+  }
+
+
+}
+
+function addUpTo(myArray, index) {
+  return index ? myArray[index] + addUpTo(myArray, --index) : myArray[index];
+}
